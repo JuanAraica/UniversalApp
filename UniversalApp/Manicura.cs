0a35f8d2a1545b0f091b7659e6ea373535a9c062
JUAN ARAICA NAVARRO <juan.araica.navarro@est.una.ac.cr>
2021-03-22 20:18:38 -0600

    Agregar archivos de proyecto.

---
+++ UniversalApp/Manicura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversalApp
+{
+    public class Manicura
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public string Empid { get; set; }
+        public string EmpName { get; set; }
+        public string Designation { get; set; }
+        public string Department { get; set; }
+    }
+}

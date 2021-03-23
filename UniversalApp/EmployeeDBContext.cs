7f204b1b91e76c0ee493e44ce76d2d27bbc5cc16
JUAN ARAICA NAVARRO <juan.araica.navarro@est.una.ac.cr>
2021-03-22 20:32:43 -0600

    Clase coneccion y paquetes nuget instalados.

---
+++ UniversalApp/EmployeeDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversalApp
+{
+    public class EmployeeDBContext:DbContext
+    {
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options)
+        : base(options)
+        { 
+        }
+public DbSet<EmployeeInfo> EmployeeInfos { get; set; }
+    }
+}
